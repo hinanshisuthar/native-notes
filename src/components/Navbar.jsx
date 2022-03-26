4e865ef4fa361e69e67fde13b85ddf22d26335b9
Hinanshi Suthar <hinanshis4@gmail.com>
2022-03-26 16:34:31 +0530

    Initial commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,50 @@
+import { BsSearch, BsPlusLg, BsFillMoonFill } from "react-icons/bs";
+import { Link } from "react-router-dom";
+import "../styles/navbar.css";
+
+const Navbar = () => {
+  return (
+    <>
+      <nav className="navigation">
+        <div className="flex-row-sb px-2 py-1">
+          <Link to="/" className="logo">
+            <div className="avatar avatar-sm">
+              <img
+                src="https://millionairedating.onluxy.com/wp-content/uploads/2018/12/woman-smilling-in-red-top-and-red-lipstick-square-e1544061815643.jpg"
+                alt="avatar-img"
+                className="img-responsive img-circular"
+              />
+            </div>
+          </Link>
+          <div className="flex-row-start nav-title">
+            <div className="mx-2">
+              <p className="text-md text-bold">All Notes</p>
+            </div>
+            <div htmlFor="search-field" className="flex-row-sb mx-2 search-field">
+              <input
+                type="text"
+                className="text-sm p-sm"
+                id="search-field"
+                placeholder="Search notes"
+              />
+              <button type="submit" className="btn-icon pr-sm">
+                <BsSearch size={20} />
+              </button>
+            </div>
+            <div className="mx-2">
+              <button className="btn btn-primary flex-row-sb">
+                {" "}
+                <BsPlusLg className="plus-icon" /> Add New Note
+              </button>
+            </div>
+          </div>
+          <div>
+            <BsFillMoonFill />
+          </div>
+        </div>
+      </nav>
+    </>
+  );
+};
+
+export { Navbar };
