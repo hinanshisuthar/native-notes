53b2085fa957b9022e747b1c62d9d796ed6443b3
Hinanshi Suthar <hinanshis4@gmail.com>
2022-03-27 19:31:13 +0530

    feat: trash page

---
+++ src/pages/TrashPage.jsx
@@ -0,0 +1,26 @@
+import { Aside } from "../components/Aside";
+import { Navbar } from "../components/Navbar";
+import { NewNote } from "../components/NewNote";
+import { BsTrash } from "../components/icons";
+
+const TrashPage = () => {
+  return (
+    <>
+      <Navbar />
+      <div className="grid grid-20-80">
+        <Aside />
+        <section className="px-1 py-2">
+          <div className="flex-row-sb pr-2 mr-1">
+            <h3 className="px-2">Trashed Notes</h3>
+            <BsTrash size={20} />
+          </div>
+          <div>
+            <NewNote />
+          </div>
+        </section>
+      </div>
+    </>
+  );
+};
+
+export { TrashPage };
