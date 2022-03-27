e5deca3a8553a520fdf76e7b6ac8d8da3d615174
Hinanshi Suthar <hinanshis4@gmail.com>
2022-03-27 19:27:31 +0530

    update: added routes and Links for all pages

--- src/App.js
+++ src/App.js
@@ -2,7 +2,10 @@ import "./App.css";
 import {Routes, Route} from 'react-router-dom'
 import { HomePage } from "./pages/HomePage";
 import { LandingPage } from "./pages/LandingPage";
-// import { LandingPage } from "./pages/LandingPage";
+import { LabelPage } from "./pages/LabelPage";
+import { ArchivePage } from "./pages/ArchivePage";
+import { TrashPage } from "./pages/TrashPage";
+import { ProfilePage } from "./pages/ProfilePage";
 
 function App() {
   return (
@@ -10,8 +13,11 @@ function App() {
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/home" element={<HomePage />} />
+        <Route path="/labels" element={<LabelPage />} />
+        <Route path="/archived" element={<ArchivePage />} />
+        <Route path="/trashed" element={<TrashPage />} />
+        <Route path="/profile" element={<ProfilePage />} />
       </Routes>
-
     </div>
   );
 }
--- src/components/Aside.jsx
+++ src/components/Aside.jsx
@@ -1,31 +1,41 @@
 import "../styles/aside.css";
-import { BsPlusLg, BsHouse, BsArchive, BsTrash } from "react-icons/bs";
-import { MdLabelOutline } from "react-icons/md";
-import { BiUserCircle } from "react-icons/bi";
+import {BsPlusLg, BsHouse, BsArchive, BsTrash, MdLabelOutline, BiUserCircle} from './icons'
+
+import { Link } from "react-router-dom";
 
 const Aside = () => {
   return (
     <div className="aside">
-      <div className="p-sm m-md flex-row-start">
-        <BsHouse size={22} className="icon home-c" />
-        Home <span className="count home ml-auto">01</span>
-      </div>
-      <div className="p-sm m-md flex-row-start">
-        <MdLabelOutline size={25} className="icon label-c" />
-        Labels <span className="count label ml-auto">01</span>
-      </div>
-      <div className="p-sm m-md flex-row-start">
-        <BsArchive size={22} className="icon archive-c" />
-        Archive <span className="count archive ml-auto">01</span>
-      </div>
-      <div className="p-sm m-md flex-row-start">
-        <BsTrash size={22} className="icon trash-c" />
-        Trash <span className="count trash ml-auto">01</span>
-      </div>
-      <div className="p-sm m-md flex-row-start">
-        <BiUserCircle size={25} className="icon profile" />
-        Profile
-      </div>
+      <Link className="router-link" to="/home">
+        <div className="p-sm m-md flex-row-start">
+          <BsHouse size={22} className="icon home-c" />
+          Home <span className="count home ml-auto">01</span>
+        </div>
+      </Link>
+      <Link className="router-link" to="/labels">
+        <div className="p-sm m-md flex-row-start">
+          <MdLabelOutline size={25} className="icon label-c" />
+          Labels <span className="count label ml-auto">01</span>
+        </div>
+      </Link>
+      <Link className="router-link" to="/archived">
+        <div className="p-sm m-md flex-row-start">
+          <BsArchive size={22} className="icon archive-c" />
+          Archive <span className="count archive ml-auto">01</span>
+        </div>
+      </Link>
+      <Link className="router-link" to="/trashed">
+        <div className="p-sm m-md flex-row-start">
+          <BsTrash size={22} className="icon trash-c" />
+          Trash <span className="count trash ml-auto">01</span>
+        </div>
+      </Link>
+      <Link className="router-link" to="/profile">
+        <div className="p-sm m-md flex-row-start">
+          <BiUserCircle size={25} className="icon profile" />
+          Profile
+        </div>
+      </Link>
       <div className="p-sm m-md">
         <button className="btn btn-primary flex-row-sb">
           {" "}
