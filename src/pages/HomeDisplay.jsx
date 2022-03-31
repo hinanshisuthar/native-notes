ba83cbf1fa62a1845bc793193dfb30ca50a43b32
Hinanshi Suthar <hinanshis4@gmail.com>
2022-04-01 03:31:38 +0530

    feat: added display for notes on home-screen

--- src/pages/HomeDisplay.jsx
+++ src/pages/HomeDisplay.jsx
@@ -1,11 +1,26 @@
 import {Filters} from '../components/Filters';
-import {NewNote} from '../components/NewNote';
+import { NewNote } from '../components/NewNote';
+import { useNotes } from '../context/notes-context';
+import {NoteCard} from '../components/NoteCard'
 
 const HomeDisplay = () => {
+  const {noteState} = useNotes();
+
     return (
         <div>
           <Filters />
-          <NewNote />
+            <NewNote />
+            {(noteState.notes.length === 0 ) ? (
+                <div className="center">
+                    <h3 className="m-md">No notes to display</h3>
+                </div>
+            ) : <div className="flex-row-start-wrap">{(noteState.notes.map((notes) => {
+              return <
+                NoteCard notes={notes} id={notes._id}/>
+          }))}</div>
+
+            
+            }
         </div>
     )
 }
