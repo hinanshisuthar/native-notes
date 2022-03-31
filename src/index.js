29b646050bad0530c2ca9c231a65522e60b31add
Hinanshi Suthar <hinanshis4@gmail.com>
2022-04-01 03:30:44 +0530

    update: added NoteProvider

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import "./index.css";
 import App from "./App";
 import { makeServer } from "./server";
 import {BrowserRouter as Router} from 'react-router-dom';
+import { NoteProvider } from "./context/notes-context";
 
 // Call make Server
 makeServer();
@@ -11,7 +12,9 @@ makeServer();
 ReactDOM.render(
   <React.StrictMode>
     <Router>
-      <App />
+      <NoteProvider>
+        <App />
+      </NoteProvider>
     </Router>
   </React.StrictMode>,
   document.getElementById("root")
