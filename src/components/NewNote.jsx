aeca2c7e6aed6e090a9fce42a8162ee5a53bafc5
Hinanshi Suthar <hinanshis4@gmail.com>
2022-04-01 03:29:57 +0530

    feat: added new note implementation

--- src/components/NewNote.jsx
+++ src/components/NewNote.jsx
@@ -1,31 +1,24 @@
-import { BsPin, BsPalette, BsTrash, MdLabelOutline, MdOutlineArchive } from "./icons";
-import "../styles/new-note.css";
+import ReactQuill from 'react-quill';
+import { useNotes } from '../context/notes-context';
+import '../styles/new-note.css'
 
 const NewNote = () => {
-  return (
-    <div className="note px-2 py-1 mx-2 my-1">
-      <div className="note-header flex-row-start p-sm">
-        <BsPin size={21} className="icon" />
-        <h4 className="note-title ml-1">My Note 1</h4>
-        <small className="note-label ml-auto text-bold">Work</small>
+    const {value, setValue, noteDispatch} = useNotes();
+    const addNoteHandler = (e) => {
+        noteDispatch({type: 'ADD_NOTES', payload: {value}});
+        setValue('')
+    }   
+    return (
+      <div className='editor-con center'>
+        <ReactQuill
+          value={value}
+          onChange={(value) => setValue(value)}
+          placeholder={"Start writing..."}
+          className="editor"
+        />
+        <button className='btn btn-secondary m-md' onClick={addNoteHandler}>Add</button>
       </div>
-      <div className="p-sm">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt,
-        officiis! Lorem ipsum dolor sit amet.
-      </div>
-      <div className="flex-row-sb p-sm">
-          <small className="text-bold">26-03-22</small>
-          <div>
-            <BsPalette size={22} className="icon" />
-            <MdLabelOutline size={22} className="icon" />
-            <MdOutlineArchive size={22} className="icon" />
-            <BsTrash size={22} className="icon" />
-          </div>
-      </div>
-      <div className="note-"></div>
-      <div className="note-"></div>
-    </div>
-  );
-};
+    )
+}
 
-export { NewNote };
+export {NewNote};
