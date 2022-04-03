ebc02781a1c75fbaac28a0e399a18ee106ec95cb
Hinanshi Suthar <hinanshis4@gmail.com>
2022-04-03 18:25:26 +0530

    refactor: renamed handler

--- src/components/NewNote.jsx
+++ src/components/NewNote.jsx
@@ -1,24 +1,37 @@
-import ReactQuill from 'react-quill';
-import { useNotes } from '../context/notes-context';
-import '../styles/new-note.css'
+import ReactQuill from "react-quill";
+import { useState } from "react";
+import { useNotes } from "../context/notes-context";
+import "../styles/new-note.css";
 
 const NewNote = () => {
-    const {value, setValue, noteDispatch} = useNotes();
-    const addNoteHandler = (e) => {
-        noteDispatch({type: 'ADD_NOTES', payload: {value}});
-        setValue('')
-    }   
-    return (
-      <div className='editor-con center'>
-        <ReactQuill
-          value={value}
-          onChange={(value) => setValue(value)}
-          placeholder={"Start writing..."}
-          className="editor"
-        />
-        <button className='btn btn-secondary m-md' onClick={addNoteHandler}>Add</button>
-      </div>
-    )
-}
+  const {
+    input,
+    setInput,
+    noteHandler,
+    noteButton,
+    noteState,
+    editNoteHandler,
+  } = useNotes();
+  const inputContent = input.content;
 
-export {NewNote};
+  // const noteExists = noteState.notes.filter((note) => note.id !== input.id);
+  // console.log(noteExists);
+
+  return (
+    <div className="editor-con center" id={input.id}>
+      <ReactQuill
+        value={inputContent}
+        onChange={(inputContent) =>
+          setInput({ ...input, content: inputContent })
+        }
+        placeholder={"Start writing..."}
+        className="editor"
+      />
+      <button className="btn btn-secondary m-md" onClick={noteHandler}>
+        {noteButton}
+      </button>
+    </div>
+  );
+};
+
+export { NewNote };
