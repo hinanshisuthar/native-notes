9d30ddf1e40953b8301c23d42ff385a0d67db1d1
Hinanshi Suthar <hinanshis4@gmail.com>
2022-04-03 18:26:56 +0530

    feat: edit note functionality

--- src/components/NoteCard.jsx
+++ src/components/NoteCard.jsx
@@ -1,25 +1,44 @@
-import { BsPin, BsPalette, BsTrash, MdLabelOutline, MdOutlineArchive } from "./icons";
+import {
+  BsPin,
+  BsPalette,
+  BsTrash,
+  MdLabelOutline,
+  MdOutlineArchive,
+} from "./icons";
 import "../styles/note-card.css";
 import parse from "react-html-parser";
+import { useNotes } from "../context/notes-context";
 
-const NoteCard = ({notes}) => {
+const NoteCard = ({ notes }) => {
+  const { setInput, setShowEditor, setNoteButton } = useNotes();
+
+  const editNote = () => {
+    setInput(notes);
+    setNoteButton("Save");
+  };
+
   return (
-    <div className="note px-2 py-1 mx-2 my-1" key={notes._id}>
+    <div className="note px-2 py-1 mx-2 my-1" key={notes.id} id={notes.id}>
       <div className="note-header flex-row-start p-sm">
         <BsPin size={21} className="icon" />
-        <small className="note-label ml-auto text-bold">Label 1</small>
+        <small className="note-label ml-auto text-bold">{notes.tag}</small>
       </div>
-      <div className="p-sm">
-        {parse(notes.value)}
+      <div
+        className="p-sm"
+        style={{ cursor: "pointer" }}
+        onClick={() => editNote()}
+      >
+        {parse(notes.content)}
       </div>
+
       <div className="flex-row-sb p-sm">
-          <small className="text-bold">{new Date().toLocaleString()}</small>
-          <div>
-            <BsPalette size={22} className="icon" />
-            <MdLabelOutline size={22} className="icon" />
-            <MdOutlineArchive size={22} className="icon" />
-            <BsTrash size={22} className="icon" />
-          </div>
+        <small className="text-bold">{notes.creationTime}</small>
+        <div>
+          <BsPalette size={22} className="icon" />
+          <MdLabelOutline size={22} className="icon" />
+          <MdOutlineArchive size={22} className="icon" />
+          <BsTrash size={22} className="icon" />
+        </div>
       </div>
       <div className="note-"></div>
       <div className="note-"></div>
