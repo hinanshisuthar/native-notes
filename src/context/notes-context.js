631fa37ed4be80c869630241294c5f2a91b5cd8c
Hinanshi Suthar <hinanshis4@gmail.com>
2022-04-04 04:37:21 +0530

    feat: implementation using api calls

--- src/context/notes-context.js
+++ src/context/notes-context.js
@@ -1,34 +1,73 @@
 import { createContext, useContext, useReducer, useState } from "react";
 import { noteReducer } from "../reducers/noteReducer";
 import { v4 as uuidv4 } from "uuid";
+import { addNoteOperation } from "../operations/noteOperations/addNoteOperation";
+import { encodedToken } from "../utils/token";
+import { editNoteOperation } from "../operations/noteOperations/editNoteOperation";
+import { useEffect } from "react";
+import { getNoteOperation } from "../operations/noteOperations/getNoteOperation";
 
 const NoteContext = createContext();
 
+const initalState = {
+  _id: '',
+  content: "",
+  backgroundColor: "red",
+  tag: [],
+};
+
 const NoteProvider = ({ children }) => {
-  const [input, setInput] = useState({
-    id: uuidv4(),
-    content: "",
-    backgroundColor: "red",
-    creationTime: `${new Date().toLocaleString()}`,
-    tag: "Tag 1",
-    isPinned: false,
-  });
-  const [showEditor, setShowEditor] = useState(false);
-  const [noteButton, setNoteButton] = useState("Add");
-
-  const noteHandler = (e) => {
-    {
-      noteButton === "Add"
-        ? (noteDispatch({ type: "ADD_NOTES", payload: input }), setInput(""))
-        : (noteDispatch({ type: "EDIT_NOTES", payload: input }),
-          setInput(""),
-          setNoteButton("Add"));
-    }
-  };
-
+  const [input, setInput] = useState(initalState);
   const [noteState, noteDispatch] = useReducer(noteReducer, {
     notes: [],
   });
+  const noteAlreadyExists = noteState.notes?.find(
+    (note) => note._id === input._id
+  );
+    // console.log(noteAlreadyExists);
+
+  const noteHandler = async () => {
+    if (noteAlreadyExists) {
+      try {
+        const { data, status } = await editNoteOperation(
+          {
+            ...noteAlreadyExists,
+            title: input.content.trim(),
+            creationTime: new Date().toLocaleString(),
+            tags: "tag name updated",
+          },
+          encodedToken
+        );
+
+        if (status === 201) {
+          noteDispatch({
+            type: "ADD_NOTES",
+            payload: data.notes,
+          });
+        }
+      } catch (err) {
+        console.error(err);
+      }
+    } else {
+      try {
+        const { data, status } = await addNoteOperation(
+          {
+            ...input,
+            creationTime: new Date().toLocaleString(),
+            tags: "tag 1",
+          },
+          encodedToken
+        );
+
+        if (status === 201) {
+          noteDispatch({ type: "ADD_NOTES", payload: data.notes });
+        }
+      } catch (err) {
+        console.error(err);
+      }
+    }
+    setInput(initalState);
+  };
 
   return (
     <NoteContext.Provider
@@ -38,10 +77,7 @@ const NoteProvider = ({ children }) => {
         input,
         setInput,
         noteHandler,
-        showEditor,
-        setShowEditor,
-        noteButton,
-        setNoteButton,
+        noteAlreadyExists,
       }}
     >
       {children}
