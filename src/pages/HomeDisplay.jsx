3c0ac0bab0e503b529c61276571171599165d32d
Hinanshi Suthar <hinanshis4@gmail.com>
2022-04-05 18:54:17 +0530

    refactor: destructuring implemented

--- src/pages/HomeDisplay.jsx
+++ src/pages/HomeDisplay.jsx
@@ -1,12 +1,16 @@
 import { Filters, NewNote } from "../components";
 import { NoteList } from "../components/NoteList";
+import { useNotes } from "../context/notes-context";
 
 const HomeDisplay = () => {
+  const {
+    noteState: { notes },
+  } = useNotes();
   return (
     <div>
       <Filters />
       <NewNote />
-      <NoteList />
+      <NoteList notes={notes} />
     </div>
   );
 };
