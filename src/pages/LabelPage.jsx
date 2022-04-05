a272700fbc7e2f4e5c8204108f7b798c282212d9
Hinanshi Suthar <hinanshis4@gmail.com>
2022-04-06 02:19:07 +0530

    update: label page to display notes w/ Labels

--- src/pages/LabelPage.jsx
+++ src/pages/LabelPage.jsx
@@ -1,10 +1,24 @@
 import { Navbar } from "../components/Navbar";
 import { Aside } from "../components/Aside";
-import { NoteCard } from "../components/NoteCard";
+import { NoteList } from "../components/NoteList";
 import "../styles/labels.css";
 import { Chips } from "../components/Chips";
+import { useState } from "react";
+import { useNotes } from "../context/notes-context";
 
 const LabelPage = () => {
+  const {
+    noteState: { notes },
+  } = useNotes();
+
+  const getTagNames = notes.reduce((acc, curr) => {
+    return acc.concat(
+      curr.tags.filter((element) => {
+        return !acc.some((item) => item === element);
+      })
+    );
+  }, []);
+
   return (
     <>
       <Navbar />
@@ -13,12 +27,16 @@ const LabelPage = () => {
         <div>
           <div className="chips-con py-1 px-2">
             <fieldset className="px-2 py-1">
-              <legend><h4>Labels</h4></legend>
+              <legend>
+                <h4>Labels</h4>
+              </legend>
               <Chips />
             </fieldset>
           </div>
-          <div>
-              <NoteCard />
+          <div className="flex-row-start-wrap">
+            {getTagNames?.map((tag) => (
+              <NoteList notes={notes.filter((note) => note.tags[0] === tag)} />
+            ))}
           </div>
         </div>
       </div>
