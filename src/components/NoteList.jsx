9faa7828acfed14209a8da4588ad69c67d645afe
Hinanshi Suthar <hinanshis4@gmail.com>
2022-04-05 18:52:35 +0530

    refactor: destructuring used

--- src/components/NoteList.jsx
+++ src/components/NoteList.jsx
@@ -1,17 +1,17 @@
 import { useNotes } from "../context/notes-context";
 import { NoteCard } from "./NoteCard";
 
-const NoteList = () => {
+const NoteList = ({ notes }) => {
   const { noteState } = useNotes();
   return (
     <div>
-      {noteState.notes.length === 0 ? (
+      {notes?.length === 0 ? (
         <div className="center">
           <h3 className="m-md">No notes to display</h3>
         </div>
       ) : (
         <div className="flex-row-start-wrap">
-          {noteState.notes.map((note, index) => {
+          {notes?.map((note, index) => {
             return <NoteCard notes={note} key={index} />;
           })}
         </div>
