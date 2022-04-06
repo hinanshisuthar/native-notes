05e11c89fd45699c7c99da3c954fa837f63fb00d
Hinanshi Suthar <hinanshis4@gmail.com>
2022-04-07 03:44:06 +0530

    feat: filters menu

--- src/components/DropdownMenu.jsx
+++ src/components/DropdownMenu.jsx
@@ -1,14 +1,79 @@
 import { useState } from "react";
 import { useNotes } from "../context/notes-context";
+import "../styles/dropdownMenu.css";
 
 const DowndownMenu = () => {
+  const {
+    noteState: { sortBy, filterCategories },
+    noteDispatch,
+  } = useNotes();
+
+  const { urgent, intermediate, trivial } = filterCategories;
+
   return (
-    <select name="tags" id="tags" className="select mx-sm text-bold">
-      <option value="priority">priority</option>
-      <option value="assigned">assigned</option>
-      <option value="flagged">flagged</option>
-      <option value="due date">due date</option>
-    </select>
+    <div className="filters-menu flex-col-start p-1">
+      <div className="flex-col">
+        <h5 className="mr-1">Sort By:</h5>
+        <label htmlFor="sort-newest">
+          <input
+            type="radio"
+            id="sort-newer"
+            name="sort"
+            className="input icon"
+            checked={sortBy === "CREATION_TIME_NEWER"}
+            onChange={() => noteDispatch({ type: "CREATION_TIME_NEWER" })}
+          />
+          Newer
+        </label>
+        <label>
+          <input
+            type="radio"
+            id="sort-older"
+            name="sort"
+            className="input icon"
+            checked={sortBy === "CREATION_TIME_OLDER"}
+            onChange={() => noteDispatch({ type: "CREATION_TIME_OLDER" })}
+          />
+          Older
+        </label>
+      </div>
+      <div className="flex-col">
+        <h5 className="mr-1">Due In:</h5>
+        <label htmlFor="sort-newest">
+          <input
+            type="checkbox"
+            id="filter-1"
+            name="filter"
+            className="input icon"
+            checked={urgent}
+            onChange={() => noteDispatch({ type: "URGENT" })}
+          />
+          1 Day
+        </label>
+        <label>
+          <input
+            type="checkbox"
+            id="filter-3"
+            name="filter"
+            className="input icon"
+            checked={intermediate}
+            onChange={() => noteDispatch({ type: "INTERMEDIATE" })}
+          />
+          2-3 Days
+        </label>
+        <label>
+          <input
+            type="checkbox"
+            id="filter-7"
+            name="filter"
+            className="input icon"
+            checked={trivial}
+            onChange={() => noteDispatch({ type: "TRIVIAL" })}
+          />
+          A week
+        </label>
+      </div>
+    </div>
   );
 };
 
@@ -25,7 +90,9 @@ const DowndownMenuTags = () => {
       onChange={(e) => setInput({ ...input, tags: [e.target.value] })}
     >
       {tags?.map((tag) => (
-        <option value={tag} key={tag}>{tag}</option>
+        <option value={tag} key={tag}>
+          {tag}
+        </option>
       ))}
     </select>
   );
