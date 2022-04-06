5a235916253683e074b875a34baf8aa8a5412033
Hinanshi Suthar <hinanshis4@gmail.com>
2022-04-06 14:49:04 +0530

    feat: archive note implementation

--- src/components/NoteCard.jsx
+++ src/components/NoteCard.jsx
@@ -4,14 +4,29 @@ import {
   BsTrash,
   MdLabelOutline,
   MdOutlineArchive,
+  MdOutlineUnarchive,
 } from "./icons";
 import "../styles/note-card.css";
 import { useNotes } from "../context/notes-context";
 import { moveToTrash } from "../operations/noteOperations/deleteNoteOperation";
+import { addToArchiveOperation } from "../operations/archiveOperations/addToArchiveOperation";
+import { unarchiveNote } from "../operations/archiveOperations/unarchiveNote";
+import { deleteFromArchive } from "../operations/archiveOperations/deleteFromArchive";
 import { encodedToken } from "../utils/token";
 
 const NoteCard = ({ notes }) => {
-  const { setInput, noteDispatch } = useNotes();
+  const {
+    setInput,
+    noteDispatch,
+    noteState: { archives, trash },
+  } = useNotes();
+  const inArchivesAlready = archives?.find(
+    (archivedNote) => archivedNote._id === notes._id
+  );
+
+  const inTrashAlready = trash?.find(
+    (trashedNote) => trashedNote._id === notes._id
+  );
 
   const editNote = () => {
     setInput(notes);
@@ -21,10 +36,54 @@ const NoteCard = ({ notes }) => {
     try {
       const { data, status } = await moveToTrash(notes, encodedToken);
 
-      if (status === 200) {
+      if (status === 200 || status === 201) {
         noteDispatch({
           type: "TRASH_NOTE",
           payload: { notes: data.notes, trash: notes },
+        });
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const addToArchive = async (notes) => {
+    try {
+      const { data, status } = await addToArchiveOperation(notes, encodedToken);
+      if (status === 200 || status === 201) {
+        noteDispatch({
+          type: "ADD_TO_ARCHIVE",
+          payload: { notes: data.notes, archives: data.archives },
+        });
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const unArchiveNote = async (notes) => {
+    try {
+      const { data, status } = await unarchiveNote(notes, encodedToken);
+      if (status === 200 || status === 201) {
+        noteDispatch({
+          type: "ADD_TO_ARCHIVE",
+          payload: { notes: data.notes, archives: data.archives },
+        });
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const deleteFromArchiveFunc = async (notes) => {
+    console.log("entering the loop");
+    try {
+      const { data, status } = await deleteFromArchive(notes, encodedToken);
+
+      if (status === 200 || status === 201) {
+        noteDispatch({
+          type: "DELETE_FROM_ARCHIVE",
+          payload: { archives: data.archives, trash: notes },
         });
       }
     } catch (err) {
@@ -51,11 +110,26 @@ const NoteCard = ({ notes }) => {
         <div>
           <BsPalette size={22} className="icon" />
           <MdLabelOutline size={22} className="icon" />
-          <MdOutlineArchive size={22} className="icon" />
+
+          {inArchivesAlready ? (
+            <MdOutlineUnarchive
+              size={22}
+              className="icon"
+              onClick={() => unArchiveNote(notes)}
+            />
+          ) : (
+            <MdOutlineArchive
+              size={22}
+              className="icon"
+              onClick={() => addToArchive(notes)}
+            />
+          )}
           <BsTrash
             size={22}
             className="icon"
-            onClick={() => trashNote(notes)}
+            onClick={() =>
+              inArchivesAlready ? deleteFromArchiveFunc(notes) : trashNote(notes)
+            }
           />
         </div>
       </div>
