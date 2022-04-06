e78441a1ccfe95569d9359e79d5a4ee64354273e
Hinanshi Suthar <hinanshis4@gmail.com>
2022-04-07 00:02:42 +0530

    feat: search by tags implementation

--- src/components/NoteList.jsx
+++ src/components/NoteList.jsx
@@ -2,16 +2,24 @@ import { useNotes } from "../context/notes-context";
 import { NoteCard } from "./NoteCard";
 
 const NoteList = ({ notes }) => {
-  const { noteState } = useNotes();
+  const { search } = useNotes();
+  const getSearchedNotes = notes.filter((note) => {
+    if (search === "") {
+      return note;
+    } else if (note.tags[0].toLowerCase().includes(search.toLowerCase())) {
+      return note;
+    }
+  });
+
   return (
     <div className="pl-md">
-      {notes?.length === 0 ? (
+      {getSearchedNotes?.length === 0 ? (
         <div className="center">
           <h3 className="m-md">No notes to display</h3>
         </div>
       ) : (
         <div className="flex-row-start-wrap">
-          {notes?.map((note, index) => {
+          {getSearchedNotes?.map((note, index) => {
             return <NoteCard notes={note} key={index} />;
           })}
         </div>
