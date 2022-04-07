5abe20f4fbbf8f0ce6e0f7f96bc5804c915036a4
Hinanshi Suthar <hinanshis4@gmail.com>
2022-04-07 23:44:03 +0530

    fix: count updated in aside nav

--- src/components/Aside.jsx
+++ src/components/Aside.jsx
@@ -2,32 +2,34 @@ import "../styles/aside.css";
 import { BsHouse, BsArchive, BsTrash, MdLabelOutline, BiUserCircle} from './icons'
 
 import { Link } from "react-router-dom";
+import { useNotes } from "../context/notes-context";
 
 const Aside = () => {
+  const {noteState: {notes, trash, archives}, tags} = useNotes();
   return (
     <div className="aside">
       <Link className="router-link" to="/home">
         <div className="p-sm m-md flex-row-start">
           <BsHouse size={22} className="icon home-c" />
-          Home <span className="count home ml-auto">01</span>
+          Home <span className="count home ml-auto">{notes.length}</span>
         </div>
       </Link>
       <Link className="router-link" to="/labels">
         <div className="p-sm m-md flex-row-start">
           <MdLabelOutline size={25} className="icon label-c" />
-          Labels <span className="count label ml-auto">01</span>
+          Labels <span className="count label ml-auto">{tags.length}</span>
         </div>
       </Link>
       <Link className="router-link" to="/archived">
         <div className="p-sm m-md flex-row-start">
           <BsArchive size={22} className="icon archive-c" />
-          Archive <span className="count archive ml-auto">01</span>
+          Archive <span className="count archive ml-auto">{archives.length}</span>
         </div>
       </Link>
       <Link className="router-link" to="/trashed">
         <div className="p-sm m-md flex-row-start">
           <BsTrash size={22} className="icon trash-c" />
-          Trash <span className="count trash ml-auto">01</span>
+          Trash <span className="count trash ml-auto">{trash.length}</span>
         </div>
       </Link>
       <Link className="router-link" to="/profile">
